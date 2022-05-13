068cd10911ad8d4d79c99151aac976a8bddef44c
dengjingjing <dengjingjing@benewtech.cn>
2022-05-13 15:38:51 +0800

    wip(axios 封装) axios 状态封装请求封装，多个接口请求封装

---
+++ src/utils/http/Axios.ts
@@ -0,0 +1,8 @@
+import type { AxiosRequestConfig, AxiosInstance, AxiosResponse, AxiosError } from 'axios'
+import type { Result, RequestOptions } from '#/axios'
+
+/**
+ * qs.parse,qs.stringify是用来处理url的
+ * JSON.parse,JSON.stringify是用来处理json格式的
+ */
+import qs from 'qs'
